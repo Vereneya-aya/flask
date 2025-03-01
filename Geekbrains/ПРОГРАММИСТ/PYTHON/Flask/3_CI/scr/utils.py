c4c0b34f079dbcb3b1efba6d8ad7fa6bd0c5a8d4
Veranika <155478407+Vereneya-aya@users.noreply.github.com>
2025-03-01 13:16:42 +0100

    Delete Geekbrains directory

--- Geekbrains/ПРОГРАММИСТ/PYTHON/Flask/3_CI/scr/utils.py
+++
@@ -1,7 +0,0 @@
-def is_adult(age):
-    """Функция проверяет, является ли человек совершеннолетним (18+)."""
-    if not isinstance(age, (int, float)):
-        raise ValueError("Возраст должен быть числом")
-    if age < 0:
-        raise ValueError("Возраст не может быть отрицательным")
-    return age >= 18
